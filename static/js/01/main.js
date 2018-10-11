965f6bcbd083422ee96ed421695ef4b3cacd5c2d
Jameschy <chig@foxmail.com>
2018-10-11 14:42:59 +0800

    1.厘清header文件中结构，删除冗余引用 2.分别切换header1和header2测试 3.调整header2中外链，删除之

--- static/js/01/main.js
+++ static/js/01/main.js
@@ -22,16 +22,16 @@
     /*----------------------------
     1. Mobile Menu Activation
     -----------------------------*/
-    jQuery('.mobile-menu nav').meanmenu({
-        meanScreenWidth: "991",
-    });
+    // jQuery('.mobile-menu nav').meanmenu({
+    //     meanScreenWidth: "991",
+    // });
 
     /*----------------------------
     2 Checkout Page Activation
     -----------------------------*/
-    $('#ship-box').on('click', function () {
-        $('#ship-box-info').slideToggle();
-    });
+    // $('#ship-box').on('click', function () {
+    //     $('#ship-box-info').slideToggle();
+    // });
 
     /*----------------------------
     3. NivoSlider Activation
@@ -52,163 +52,163 @@
     /*----------------------------------------------------
     4. Collection Activation
     -----------------------------------------------------*/
-    $('.kiosk-collection').owlCarousel({
-        loop: false,
-        nav: true,
-        dots: false,
-        smartSpeed: 1000,
-
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-        margin: 30,
-        responsive: {
-            0: {
-                items: 1,
-                autoplay: true
-            },
-            480: {
-                items: 2
-            },
-            768: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            }
-        }
-
-    })
+    // $('.kiosk-collection').owlCarousel({
+    //     loop: false,
+    //     nav: true,
+    //     dots: false,
+    //     smartSpeed: 1000,
+    //
+    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    //     margin: 30,
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //             autoplay: true
+    //         },
+    //         480: {
+    //             items: 2
+    //         },
+    //         768: {
+    //             items: 2
+    //         },
+    //         1000: {
+    //             items: 3
+    //         },
+    //         1200: {
+    //             items: 4
+    //         }
+    //     }
+    //
+    // })
 
     
 
     /*----------------------------------------------------
     6. Related Product Activation
     -----------------------------------------------------*/
-    $('.kiosk-related-product').owlCarousel({
-        loop: false,
-        nav: true,
-        dots: false,
-        smartSpeed: 1000,
-
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-        margin: 30,
-        responsive: {
-            0: {
-                items: 1,
-                autoplay: true
-            },
-            480: {
-                items: 2
-            },
-            768: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            }
-        }
-
-    })
+    // $('.kiosk-related-product').owlCarousel({
+    //     loop: false,
+    //     nav: true,
+    //     dots: false,
+    //     smartSpeed: 1000,
+    //
+    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    //     margin: 30,
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //             autoplay: true
+    //         },
+    //         480: {
+    //             items: 2
+    //         },
+    //         768: {
+    //             items: 2
+    //         },
+    //         1000: {
+    //             items: 3
+    //         },
+    //         1200: {
+    //             items: 4
+    //         }
+    //     }
+    //
+    // })
 
     /*----------------------------------------------------
     7. Top Product Activation
     -----------------------------------------------------*/
-    $('.kiosk-top-active').owlCarousel({
-        loop: false,
-        nav: true,
-        dots: false,
-        smartSpeed: 1200,
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-        margin: 30,
-        responsive: {
-            0: {
-                items: 1,
-                // autoplay:true
-            },
-            480: {
-                items: 2,
-                autoplay: true
-            },
-            768: {
-                items: 3,
-                autoplay: true
-            },
-            992: {
-                items: 3,
-                autoplay: true
-            },
-            1200: {
-                items: 5,
-                autoplay:true
-            }
-        }
-    })
+    // $('.kiosk-top-active').owlCarousel({
+    //     loop: false,
+    //     nav: true,
+    //     dots: false,
+    //     smartSpeed: 1200,
+    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    //     margin: 30,
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //             // autoplay:true
+    //         },
+    //         480: {
+    //             items: 2,
+    //             autoplay: true
+    //         },
+    //         768: {
+    //             items: 3,
+    //             autoplay: true
+    //         },
+    //         992: {
+    //             items: 3,
+    //             autoplay: true
+    //         },
+    //         1200: {
+    //             items: 5,
+    //             autoplay:true
+    //         }
+    //     }
+    // })
 
     /*----------------------------------------------------
     8. Brand Banner Activation
     -----------------------------------------------------*/
-    $('.kiosk-brand').on('changed.owl.carousel initialized.owl.carousel', function (event) {
-        $(event.target)
-
-            .find('.owl-item').removeClass('last')
-            .eq(event.item.index + event.page.size - 1).addClass('last');
-
-        $(event.target)
-            .find('.owl-item').removeClass('first')
-            .eq(event.item.index).addClass('first')
-
-
-    }).owlCarousel({
-        loop: false,
-        nav: false,
-        dots: false,
-        smartSpeed: 1200,
-        margin: 1,
-        responsive: {
-            0: {
-                items: 1,
-                autoplay: true
-            },
-            480: {
-                items: 3
-            },
-            768: {
-                items: 4
-            },
-            1000: {
-                items: 5
-            }
-        }
-    })
+    // $('.kiosk-brand').on('changed.owl.carousel initialized.owl.carousel', function (event) {
+    //     $(event.target)
+    //
+    //         .find('.owl-item').removeClass('last')
+    //         .eq(event.item.index + event.page.size - 1).addClass('last');
+    //
+    //     $(event.target)
+    //         .find('.owl-item').removeClass('first')
+    //         .eq(event.item.index).addClass('first')
+    //
+    //
+    // }).owlCarousel({
+    //     loop: false,
+    //     nav: false,
+    //     dots: false,
+    //     smartSpeed: 1200,
+    //     margin: 1,
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //             autoplay: true
+    //         },
+    //         480: {
+    //             items: 3
+    //         },
+    //         768: {
+    //             items: 4
+    //         },
+    //         1000: {
+    //             items: 5
+    //         }
+    //     }
+    // })
 
     /*----------------------------------------------------
     9. Blog Activation
     -----------------------------------------------------*/
-    $('.kiosk-blog').owlCarousel({
-        loop: false,
-        nav: true,
-        dots: false,
-        smartSpeed: 1000,
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-        margin: 30,
-        responsive: {
-            0: {
-                items: 1,
-                autoplay: true
-            },
-            768: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            }
-        }
-    })
+    // $('.kiosk-blog').owlCarousel({
+    //     loop: false,
+    //     nav: true,
+    //     dots: false,
+    //     smartSpeed: 1000,
+    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    //     margin: 30,
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //             autoplay: true
+    //         },
+    //         768: {
+    //             items: 2
+    //         },
+    //         1000: {
+    //             items: 3
+    //         }
+    //     }
+    // })
 
     /*----------------------------
     10. WOW Js Activation
@@ -244,17 +244,17 @@
     /*----------------------------
     13. Price Slider Activation
     -----------------------------*/
-    $("#slider-range").slider({
-        range: true,
-        min: 0,
-        max: 80,
-        values: [20, 55],
-        slide: function (event, ui) {
-            $("#amount").val("$" + ui.values[0] + "  $" + ui.values[1]);
-        }
-    });
-    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
-        "  $" + $("#slider-range").slider("values", 1));
+    // $("#slider-range").slider({
+    //     range: true,
+    //     min: 0,
+    //     max: 80,
+    //     values: [20, 55],
+    //     slide: function (event, ui) {
+    //         $("#amount").val("$" + ui.values[0] + "  $" + ui.values[1]);
+    //     }
+    // });
+    // $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+    //     "  $" + $("#slider-range").slider("values", 1));
 
 
 })(jQuery);
