ee20357822542b62a63afcf48b5f3af363bae81c
Jameschy <chig@foxmail.com>
2018-12-03 19:08:49 +0800

    1.分离header、banner、side-bar css，2优化side-bar menu结构，3新增font-awesome字体图标库

---
+++ demo2/js/banner-fn.js
@@ -0,0 +1,78 @@
+// banner控制
+
+var swiper1 = new Swiper('#content .swiper-container',{
+
+    // var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: 500,
+    lazy: {
+        // loadPrevNext: true,
+    },
+    // effect: 'fade',
+    // effect : 'coverflow',
+    // effect: 'flip',
+    // effect: 'cube',
+    // effect: 'fade',
+    // 			fadeEffect: {
+    // 				crossFade: false,
+    // 			}
+
+    // effect : 'coverflow',
+    //   slidesPerView: 3,
+    //   centeredSlides: true,
+    //   coverflowEffect: {
+    //     rotate: 30,
+    //     stretch: 10,
+    //     depth: 60,
+    //     modifier: 2,
+    //     slideShadows : true
+    //   },
+    // })
+
+    speed: 500,
+    longSwipes: false,
+    autoplay: {
+    	disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+//0.625   5/8
+function homeResize() {
+
+    WinHeight = $(window).height();
+    WinWidth = $(window).width();
+    fullBox = $(".swiper-slide .full");
+
+    if (WinWidth > 768) {
+        if (WinWidth / WinHeight > 1.6) {
+            fullBox.width("100%").height(WinWidth * 0.625).css({
+                "position": 'absolute',
+                "left": "0px",
+                "top": (WinHeight - fullBox.height()) / 2 + "px"
+            });
+        } else {
+            fullBox.width(WinHeight * 1.6).height("100%").css({
+                "position": 'absolute',
+                "left": "-" + (fullBox.width() - WinWidth) / 2 + "px",
+                "top": "0px"
+            });
+        }
+    } else {
+        fullBox.width("100%").height("auto").css({
+            "position": 'static',
+        });
+    }
+}
+
+homeResize();
+
+$(window).resize(function() {
+    homeResize();
+});
---
+++ demo2/js/header-fn.js
@@ -0,0 +1,22 @@
+// header控制
+    $(document).ready(function() {
+        $(".menu-btn-box").click(function() {
+            // $('.slide-menu-box').toggleClass("dis-bl");
+            $('.side-left-bar').toggleClass("wid-15");
+            $('.main-center-bar').toggleClass("wid-85");
+            $('.side-left-bar').toggleClass("v-visble");
+            $('.side-left-bar').toggleClass("fl-left");
+            $('#header').toggleClass("fl-right");
+            $('#header').toggleClass("wid-85");
+            // $('.slide-menu-box').toggleClass("zindex-1");
+            // $('.msg-box').toggleClass("mb-on");
+            // $('.mb-menu').toggleClass("mb-off");
+            // $('.content-box').toggleClass("zindex-20");
+            $('.main-center-bar').toggleClass("fl-left");
+
+        });
+
+        // header tips滚动
+        SlideDirection(("#headInfor"), 2, 500, 5000);
+
+    });
