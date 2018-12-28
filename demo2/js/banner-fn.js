59272df9acfae3815b105c30f9efb7063e5ddc4b
Jameschy <chig@foxmail.com>
2018-12-28 11:14:44 +0800

    1.新建产品列表图片库；2新建产品分类模块自定义模块 3.优化banner动画PC端遮盖问题

--- demo2/js/banner-fn.js
+++ demo2/js/banner-fn.js
@@ -50,25 +50,25 @@ function homeResize() {
     WinWidth = $(window).width();
     fullBox = $(".swiper-slide .full");
 
-    if (WinWidth > 768) {
-        if (WinWidth / WinHeight > 1.6) {
-            fullBox.width("100%").height(WinWidth * 0.625).css({
-                "position": 'absolute',
-                "left": "0px",
-                "top": (WinHeight - fullBox.height()) / 2 + "px"
-            });
-        } else {
-            fullBox.width(WinHeight * 1.6).height("100%").css({
-                "position": 'absolute',
-                "left": "-" + (fullBox.width() - WinWidth) / 2 + "px",
-                "top": "0px"
-            });
-        }
-    } else {
-        fullBox.width("100%").height("auto").css({
-            "position": 'static',
-        });
-    }
+    // if (WinWidth > 768) {
+    //     if (WinWidth / WinHeight > 1.6) {
+    //         fullBox.width("100%").height(WinWidth * 0.625).css({
+    //             "position": 'absolute',
+    //             "left": "0px",
+    //             "top": (WinHeight - fullBox.height()) / 2 + "px"
+    //         });
+    //     } else {
+    //         fullBox.width(WinHeight * 1.6).height("100%").css({
+    //             "position": 'absolute',
+    //             "left": "-" + (fullBox.width() - WinWidth) / 2 + "px",
+    //             "top": "0px"
+    //         });
+    //     }
+    // } else {
+    //     fullBox.width("100%").height("auto").css({
+    //         "position": 'static',
+    //     });
+    // }
 }
 
 homeResize();
