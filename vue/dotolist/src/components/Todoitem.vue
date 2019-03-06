53eccecf0bb4dd17a5f5cac3fe00105eee0ea444
jameschy <chig@foxmail.com>
2019-03-06 17:33:45 +0800

    1.npm webpack + vue项目，测试局部样式全局样式等

---
+++ vue/dotolist/src/components/Todoitem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="item" @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+     props: [ 'content', 'index' ],
+     methods: {
+     handleDelete () {
+       this.$emit('delete', this.index)
+     }
+   }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--scoped 作用域修饰符，只对当前组件生效-->
+<!--如果不用scoped，父组件和子组件含同名class时，父组件样式会被子组件污染，会全局生效；-->
+<!--添加scoped，子组件样式就只对子组件生效-->
+<style scoped>
+.item{
+  color: green;
+}
+</style>
